c6da2c74b279115ebed5aa9bae371992c81b1638
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-01-23 20:26:49 +1100

    Complete Update Model and Test

--- src/main/java/com/hibernate/service/Update.java
+++ src/main/java/com/hibernate/service/Update.java
@@ -1,4 +1,50 @@
 package com.hibernate.service;
 
-public class Update {
+import com.hibernate.model.CustomerDetails;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+class Update {
+    // Create an EntityManagerFactory when you start the application.
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+            .createEntityManagerFactory("ORMHibernate");
+
+    static void update(final int id, final String name, final String address) {
+        // Create an EntityManager
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+
+        try {
+            // Get a transaction
+            transaction = manager.getTransaction();
+            // Begin the transaction
+            transaction.begin();
+
+            // Get the Student object
+            CustomerDetails customer = manager.find(CustomerDetails.class, id);
+
+            // Change the values
+            customer.setName(name);
+            customer.setAddress(address);
+
+            // Update the student
+            manager.persist(customer);
+
+            // Commit the transaction
+            transaction.commit();
+        } catch (Exception ex) {
+            // If there are any exceptions, roll back the changes
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            // Print the Exception
+            ex.printStackTrace();
+        } finally {
+            // Close the EntityManager
+            manager.close();
+        }
+    }
 }
